181062fe1417456b164bc08799f668662514f878
Jeff Gunderson <omgjeff@gmail.com>
2014-07-14 18:31:05 -0500

    removing some unneccessary directives and changing from bind to set to stop neverending loops of syncing with firebase

--- public/application/user/user.js
+++ public/application/user/user.js
@@ -3,7 +3,7 @@ app.controller('User', ['$scope','$timeout','$firebase', 'authFactory',
 
         $scope.auth = authFactory;
 
-        $scope.$watch( 'auth.user', function (newValue, oldValue) {
+        $scope.$watch( 'auth.user', function () {
 
             if ( $scope.auth.user ) {
 
@@ -12,43 +12,10 @@ app.controller('User', ['$scope','$timeout','$firebase', 'authFactory',
 
                 var userRef = new Firebase('https://closetickets.firebaseio.com/users/' + $scope.auth.user.id );
                 $scope.fbUser = $firebase( userRef );
-                $scope.fbUser.$bind( $scope, 'auth.user' );
+                $scope.fbUser.$set( $scope.auth.user );
             }
 
         });
+
     }
 ]);
-
-app.directive('signup', function() {
-
-    return {
-        restrict: 'E',
-        link: function( scope, element, attrs ) {
-
-            element.bind('click', function() {
-
-                scope.signUp();
-
-            });
-
-        }
-    }
-
-});
-
-app.directive('login', function() {
-
-    return {
-        restrict: 'E',
-        link: function( scope, element, attrs ) {
-
-            element.bind('click', function() {
-
-                scope.login();
-
-            });
-
-        }
-    }
-
-});
