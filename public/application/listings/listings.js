3d89a6ab41c071e37621ba50cdebc1b89a9be61a
Jeff Gunderson <omgjeff@gmail.com>
2014-07-23 23:45:14 -0500

    Changing ticket creation from the sidebar to editing the listing preview directly

--- public/application/listings/listings.js
+++ public/application/listings/listings.js
@@ -1,36 +1,3 @@
-/**
- * CONTROLLER
- */
-
-app.controller( 'Listings', [ '$scope', 'listingsService', '$firebase', 'geolocation','$timeout','authFactory','listingsService',
-    function( $scope, listingsService, $firebase, geolocation, $timeout, authFactory, listingsService ) {
-
-        $scope.auth = authFactory;
-
-        $scope.listing = {};
-
-        $scope.saveListing = function() {
-
-            if ( $scope.auth.user ) {
-
-                // add some needed user info
-                $scope.listing.userId = $scope.auth.userId;
-                $scope.listing.userName = $scope.auth.displayName;
-                $scope.listing.userImage =  $scope.auth.user.thirdPartyUserData.picture.data.url;
-
-                listingsService.$add( $scope.listing );
-            }
-            else{
-                console.log('no can do');
-            }
-
-        };
-
-        $scope.listings = listingsService;
-        $scope.listings.$bind( $scope, 'listings' );
-
-    }
-]);
 
 /**
  * SERVICES
@@ -39,8 +6,50 @@ app.controller( 'Listings', [ '$scope', 'listingsService', '$firebase', 'geoloca
 app.factory('listingsService', ['$firebase',
     function( $firebase ) {
 
-        var ref = new Firebase('https://closetickets.firebaseio.com/listings');
+        var ref = new Firebase('https://closetickets.firebaseio.com/listings' );
         return $firebase( ref );
+
+    }
+]);
+
+
+/**
+ * CONTROLLER
+ */
+
+app.controller( 'Listings', [ '$scope', 'listingsService', '$firebase', 'geolocation','$timeout','authFactory',
+    function( $scope, listingsService, $firebase, geolocation, $timeout, authFactory ) {
+
+        // get the listings and bind them
+        $scope.listing = {};
+        $scope.listings = listingsService;
+        $scope.listings.$bind( $scope, 'listings' );
+
+        // Create a listing
+        $scope.createListing = function() {
+
+            if ( $scope.auth.user ) {
+
+                // add some needed user info
+                $scope.listing.userId = $scope.auth.userId;
+                $scope.listing.userName = $scope.auth.displayName;
+                $scope.listing.userImage =  $scope.auth.user.thirdPartyUserData.picture.data.url;
+
+                listingsService.$add( $scope.listing ).then(function() {
+                    $scope.listing = {};
+                    $scope.showSell = false;
+                });
+
+            }
+
+        }
+
+        // Cancel listing
+        $scope.cancelListing = function() {
+
+            $scope.showSell = false;
+
+        }
 
     }
 ]);
@@ -49,22 +58,6 @@ app.factory('listingsService', ['$firebase',
 /**
  * DIRECTIVES
  */
-
-app.directive('createListing', function( authFactory ) {
-    return {
-        restrict: 'E',
-        controller: 'Listings',
-        templateUrl: '/application/listings/create-listing.html',
-        link: function( $scope ) {
-
-            $scope.namePlaceholder = 'Name';
-            $scope.pricePlaceholder = 'Price';
-            $scope.descriptionPlaceholder = 'Description...';
-            $scope.error = null;
-
-        }
-    }
-});
 
 app.directive('findListing', function() {
     return {
@@ -77,13 +70,74 @@ app.directive('findListing', function() {
     }
 });
 
-app.directive('listingDisplay', function() {
+app.directive('createListing', function() {
     return {
         restrict: 'E',
         controller: 'Listings',
-        templateUrl: '/application/listings/listing-display.html',
+        templateUrl: '/application/listings/create-listing.html',
         link: function ( $scope ) {
 
         }
     }
 });
+
+// TODO: MOVE
+app.directive('numberOnly', function( $filter ) {
+    return{
+        restrict: 'A',
+        link: function( $scope, $element, $attrs ) {
+
+            $element.bind('keyup', function() {
+
+                $element.val( $element.val().replace(/[^0-9.$]/g,'') );
+
+                if (  $element.val().substring( 0, 1 ) != '$' ) {
+
+                    $element.val( '$' + $element.val() );
+
+                }
+
+            });
+
+
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//app.directive('createListing', function() {
+//    return {
+//        restrict: 'E',
+//        controller: 'Listings',
+//        templateUrl: '/application/listings/create-listing.html',
+//        link: function( $scope ) {
+//
+//            $scope.namePlaceholder = 'Name';
+//            $scope.pricePlaceholder = 'Price';
+//            $scope.descriptionPlaceholder = 'Description...';
+//            $scope.error = null;
+//
+//            $scope.open = function( $event ) {
+//                $event.preventDefault();
+//                $event.stopPropagation();
+//                $scope.opened = true;
+//            };
+//
+//            $scope.initDate = new Date('2016-15-20');
+//            $scope.format = 'MMMM dd, yyyy';
+//        }
+//    }
+//});
