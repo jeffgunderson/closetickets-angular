e9f4473b8245f0c5d64a5aff21ed4187821e6f1a
Jeff Gunderson <omgjeff@gmail.com>
2014-07-23 23:41:07 -0500

    Filter for reversing the results from firebase

---
+++ public/application/filters/filters.js
@@ -0,0 +1,22 @@
+/**
+ * Reverses the results from firebase so newest shows up first in UI
+ */
+app.filter('reverse', function() {
+    function toArray(list) {
+        var k, out = [];
+        if( list ) {
+            if( angular.isArray(list) ) {
+                out = list;
+            }
+            else if( typeof(list) === 'object' ) {
+                for (k in list) {
+                    if (list.hasOwnProperty(k)) { out.push(list[k]); }
+                }
+            }
+        }
+        return out;
+    }
+    return function(items) {
+        return toArray(items).slice().reverse();
+    };
+});
