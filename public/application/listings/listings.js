205c24d309880510338c35f21a409b94877b0dc7
Jeff Gunderson <omgjeff@gmail.com>
2014-07-30 11:21:10 -0500

    Refactored listing removing the controller and sharing data using a service instead. Keeping all model data in service and will do any business logic there.

--- public/application/listings/listings.js
+++ public/application/listings/listings.js
@@ -3,53 +3,22 @@
  * SERVICES
  */
 
-app.factory('listingsService', ['$firebase',
-    function( $firebase ) {
+app.factory('listingsService', ['$firebase','$timeout',
+    function( $firebase, $timeout ) {
 
-        var ref = new Firebase('https://closetickets.firebaseio.com/listings' );
-        return $firebase( ref );
+        var fbRef = new Firebase('https://closetickets.firebaseio.com/listings' );
 
-    }
-]);
+        var public = {
 
+            listings: function() {
+                return $firebase( fbRef );
+            },
 
-/**
- * CONTROLLER
- */
+            activeListing: null
 
-app.controller( 'Listings', [ '$scope', 'listingsService', '$firebase', 'geolocation','$timeout','authFactory',
-    function( $scope, listingsService, $firebase, geolocation, $timeout, authFactory ) {
+        };
 
-        // get the listings and bind them
-        $scope.listing = {};
-        $scope.listings = listingsService;
-        $scope.listings.$bind( $scope, 'listings' );
-
-        // Create a listing
-        $scope.createListing = function() {
-
-            if ( $scope.auth.user ) {
-
-                // add some needed user info
-                $scope.listing.userId = $scope.auth.userId;
-                $scope.listing.userName = $scope.auth.displayName;
-                $scope.listing.userImage =  $scope.auth.user.thirdPartyUserData.picture.data.url;
-
-                listingsService.$add( $scope.listing ).then(function() {
-                    $scope.listing = {};
-                    $scope.showSell = false;
-                });
-
-            }
-
-        }
-
-        // Cancel listing
-        $scope.cancelListing = function() {
-
-            $scope.showSell = false;
-
-        }
+        return public;
 
     }
 ]);
@@ -59,85 +28,118 @@ app.controller( 'Listings', [ '$scope', 'listingsService', '$firebase', 'geoloca
  * DIRECTIVES
  */
 
-app.directive('findListing', function() {
+app.directive('findListing', ['listingsService', function( listingsService ) {
     return {
         restrict: 'E',
-        controller: 'Listings',
         templateUrl: '/application/listings/find-listing.html',
         link: function( $scope ) {
 
+            $scope.listings = listingsService.listings();
+            $scope.listings.$bind( $scope, 'listings' );
+
+            $scope.displayListing = function( listing ) {
+
+                listingsService.activeListing = listing;
+
+            }
+
         }
     }
-});
+}]);
 
-app.directive('createListing', function() {
+app.directive('listing', [ 'listingsService' , function( listingsService ) {
     return {
         restrict: 'E',
-        controller: 'Listings',
+        replace: true,
+        templateUrl: '/application/listings/listing.html',
+        link: function ( $scope ) {
+
+            $scope.$watch( function() {
+                    return listingsService.activeListing
+                },
+                function(newValue, oldValue) {
+                    $scope.activeListing = newValue;
+                }
+            );
+
+            $scope.closeListing = function() {
+                listingsService.activeListing = null;
+            }
+
+            $scope.messageUser = function( userId ) {
+
+            }
+
+        }
+    }
+}]);
+
+
+app.directive('createListing', [ 'listingsService','authFactory', function( listingsService, authFactory ) {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            showSell: '='
+        },
         templateUrl: '/application/listings/create-listing.html',
         link: function ( $scope ) {
 
+            $scope.auth = authFactory;
+
+            // Create a listing
+            $scope.createListing = function() {
+
+                if ( $scope.auth.user ) {
+
+                    // add some needed user info
+                    $scope.listing.userId = $scope.auth.user.id;
+                    $scope.listing.userName = $scope.auth.user.displayName;
+                    $scope.listing.userImage =  $scope.auth.user.thirdPartyUserData.picture.data.url;
+
+                    listingsService.listings.$add( $scope.listing ).then(function() {
+                        $scope.listing = {};
+                        $scope.showSell = false;
+                    });
+
+                }
+
+            }
+
+            // Cancel listing
+            $scope.cancelListing = function() {
+
+                // hide the listing creation popout
+                $scope.showSell = false;
+                // and clear the listing scope
+                $scope.listing = {};
+
+            }
+
         }
     }
-});
+}]);
+
+
+
+
+
+
+
+
+
 
 // TODO: MOVE
-app.directive('numberOnly', function( $filter ) {
+app.directive( 'numberOnly' , function() {
     return{
         restrict: 'A',
         link: function( $scope, $element, $attrs ) {
 
             $element.bind('keyup', function() {
-
-                $element.val( $element.val().replace(/[^0-9.$]/g,'') );
-
-                if (  $element.val().substring( 0, 1 ) != '$' ) {
-
-                    $element.val( '$' + $element.val() );
-
-                }
-
+                $element.val( $element.val().replace( /[^0-9.$]/g, '' ) );
             });
 
 
         }
     }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//app.directive('createListing', function() {
-//    return {
-//        restrict: 'E',
-//        controller: 'Listings',
-//        templateUrl: '/application/listings/create-listing.html',
-//        link: function( $scope ) {
-//
-//            $scope.namePlaceholder = 'Name';
-//            $scope.pricePlaceholder = 'Price';
-//            $scope.descriptionPlaceholder = 'Description...';
-//            $scope.error = null;
-//
-//            $scope.open = function( $event ) {
-//                $event.preventDefault();
-//                $event.stopPropagation();
-//                $scope.opened = true;
-//            };
-//
-//            $scope.initDate = new Date('2016-15-20');
-//            $scope.format = 'MMMM dd, yyyy';
-//        }
-//    }
-//});
+});
